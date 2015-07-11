d18e2b986ce3430639a2eabc3a0623aece94f261
rohith <rohithtp@gmail.com>
2015-07-11 19:32:19 +0530

    Added nested class for testing serialization.

--- src/test/java/com/code/examples/intro/SerializationExampleTest.java
+++ src/test/java/com/code/examples/intro/SerializationExampleTest.java
@@ -3,8 +3,11 @@ package com.code.examples.intro;
 import com.code.examples.intro.serialization.SerializationExample;
 import org.junit.Assert;
 
-
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
 
 
 /**
@@ -25,6 +28,55 @@ public class SerializationExampleTest {
         InputStream inStream = new ByteArrayInputStream(personString.getBytes());
         Person resultPerson = example.read(inStream, person);
         Assert.assertEquals(resultPerson.getName(), person.getName());
+    }
+
+    @org.junit.Test
+    public void testSerializationNested() throws IOException, ClassNotFoundException {
+        Room room = new Room();
+        Room.Door[] doors = new Room.Door[1];
+        doors[0] = (new Room().new Door(State.OPEN));
+        room.setDoors(doors);
+        SerializationExample example = new SerializationExample();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        example.write(out, room);
+        String personString = new String(out.toByteArray());
+        InputStream inStream = new ByteArrayInputStream(personString.getBytes());
+        Room result = example.read(inStream, room);
+        Assert.assertNotNull(result);
+        for (Room.Door doorElem : room.getDoors()) {
+            Assert.assertNotSame(doorElem.getState(), State.CLOSED);
+        }
+    }
+}
+
+enum State {OPEN, CLOSED};
+
+class Room implements  Serializable {
+    class Door implements  Serializable {
+        private transient State state;
+
+        public Door(State state) {
+            super();
+            setState(state);
+        }
+
+        public State getState() {
+            return state;
+        }
+
+        public void setState(State state) {
+            this.state = state;
+        }
+    }
+
+    private Door[] doors;
+
+    public Door[] getDoors() {
+        return doors;
+    }
+
+    public void setDoors(Door[] doors) {
+        this.doors = doors;
     }
 }
 
